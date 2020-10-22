4b716404ec9b4d8eaa8ce7037bdd385fed7e9940
Yoon Su Hyeon <96-05-03@naver.com>
2020-10-22 20:43:20 +0900

    2020-10-22

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,classes = Configuration.class)
+)
+@Component
+public class AutoAppConfig {
+
+}
