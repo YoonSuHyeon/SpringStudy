5bf97d19b6c6a15d853bafda6dc2e91cb4f48ef4
Yoon Su Hyeon <96-05-03@naver.com>
2021-01-19 21:39:34 +0900

    엔티티 클래스 개발

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Getter
+@Setter
+@DiscriminatorValue("M")
+public class Movie extends Item {
+    private String director;
+    private String actor;
+}
